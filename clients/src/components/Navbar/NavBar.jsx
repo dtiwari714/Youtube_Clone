47ac90d7cc4821d7f390f9484b133958c65b23b1
Durgesh Tiwari <dtiwari714@rku.ac.in>
2023-12-27 20:57:03 +0530

    Navbar:SearchBar activity and sign in Static data

--- clients/src/components/Navbar/NavBar.jsx
+++ clients/src/components/Navbar/NavBar.jsx
@@ -4,13 +4,23 @@ import logo from "../Navbar/logo.ico";
 import SearchBar from "./SearchBar/SearchBar";
 import { RiVideoAddLine } from "react-icons/ri";
 import { IoIosNotifications } from "react-icons/io";
-import { FaUserCircle } from "react-icons/fa";
+// import { FaUserCircle } from "react-icons/fa";
+import { IoApps } from "react-icons/io5";
+import { BiUserCircle } from "react-icons/bi";
 
 function NavBar() {
+  // const CurrentUser = null;
+  const CurrentUser = {
+    result: {
+      name:"Durgesh Tiwari",
+      email: "dtiwari714@rku.ac.in",
+      joinedOn: "2222-07-15T09:57:23.489Z",
+    },
+  };
   return (
     <div className="Container_Navbar">
       <div className="Burger_Logo_Navbar">
-      <div className="burger">
+        <div className="burger">
           <p></p>
           <p></p>
           <p></p>
@@ -22,12 +32,36 @@ function NavBar() {
         </div>
       </div>
       {/* <div className="Header_Search"> */}
-        <SearchBar />
+      <SearchBar />
       {/* </div> */}
       <div className="Header_icons">
         <RiVideoAddLine size={22} className={"vid_bell_Navbar"} />
+        <IoApps size={22} className={"app_navbar"} />
         <IoIosNotifications size={22} className={"notification_Navbar"} />
-        <FaUserCircle size={22} className={"user_Navbar"} />
+        {/* <FaUserCircle size={22} className={"user_Navbar"} /> */}
+      </div>
+
+      <div className="Auth_cont_Navbar">
+        {CurrentUser ? (
+          <>
+            <div className="Chanel_logo_App">
+              <p className="fstChar_logo_App">
+                {CurrentUser?.result.name ? (
+                  <>{CurrentUser?.result.name.charAt(0).toUpperCase()}</>
+                ) : (
+                  <>{CurrentUser?.result.email.charAt(0).toUpperCase()}</>
+                )}
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="Auth_Btn">
+              <BiUserCircle size={22} className={"user_navbar"} />
+              <b className="button_signIn">Sign In</b>
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
--- clients/src/components/Navbar/SearchBar/SearchBar.jsx
+++ clients/src/components/Navbar/SearchBar/SearchBar.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 import { FaSearch } from "react-icons/fa";
 import { BsMicFill } from "react-icons/bs";
+import SearchList from "./SearchList";
 
 function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [seachListA, setSeachList] = useState(false);
+  const TitleArray = ["Video1", "Animation", "Movies", "Video2"].filter((q) =>
+    q.toUpperCase().includes(searchQuery.toUpperCase())
+  );
   return (
     <>
-      <div className="SearchBar_Container">
+      <div className="SearchBar_Container"> 
         <div className="SearchBar_Container2">
           <div className="search_div">
             <input
               type="text"
               className="iBox_SearchBar"
               placeholder="Search"
+              onChange={(e) => setSearchQuery(e.target.value)}
+              value={searchQuery}
+              onClick={(e) => setSeachList(true)}
             />
-                      <FaSearch className="searchIcon_SearchBar" />
-          <BsMicFill className="Mic_SearchBar" />
+            <FaSearch className="searchIcon_SearchBar" />
+            <BsMicFill className="Mic_SearchBar" />
+            {searchQuery && seachListA && (
+              <SearchList
+                setSearchQuery={setSearchQuery}
+                TitleArray={TitleArray}
+              />
+            )}
           </div>
-
         </div>
       </div>
     </>

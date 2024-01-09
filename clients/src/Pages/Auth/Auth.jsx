1e58c303fc118c653cbcc3a14543e87f8281a469
Durgesh Tiwari <dtiwari714@rku.ac.in>
2024-01-09 16:02:57 +0530

    Auth Page part-1 && Part -2 Youtube Clone

---
+++ clients/src/Pages/Auth/Auth.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Auth.css";
+import { useDispatch } from "react-redux";
+import { GoogleLogout } from "react-google-login";
+import { BiLogOut } from "react-icons/bi";
+import { setCurrentUser } from "../../Actions/currentUser";
+// import { Link } from "react-router-dom";
+
+function Auth({ User, setAuthBtn }) {
+  const dispatch = useDispatch();
+  const onLogOutSuccess = () => {
+    dispatch(setCurrentUser(null));
+    alert("Log Out SuccessFully");
+  };
+  return (
+    <>
+      <div className="Auth_container" onClick={() => setAuthBtn(false)}>
+        <div className="Auth_container2">
+          <p className="User_Details">
+            <div className="Chanel_logo_App">
+              <p className="fstChar_logo_App">
+                {User?.result.name ? (
+                  <>{User?.result.name.charAt(0).toUpperCase()} </>
+                ) : (
+                  <>{User?.result.email.charAt(0).toUpperCase()} </>
+                )}
+              </p>
+            </div>
+            <div className="email_Auth">{User?.result.email}</div>
+          </p>
+          <div className="btns_Auth">
+            <>
+              <input
+                type="submit"
+                className="btn_Auth"
+                value="Create Your Chanel"
+                // onClick={() => setEditCreateChanelBtn(true)}
+              />
+            </>
+
+            <div>
+              <GoogleLogout
+                clientId={
+                  "350764630254-24a99f0k7r7loe703360gk8j0hk7ivhc.apps.googleusercontent.com"
+                }
+                onLogoutSuccess={onLogOutSuccess}
+                render={(renderProps) => (
+                  <div onClick={renderProps.onClick} className="btn_Auth">
+                    <BiLogOut />
+                    Log Out
+                  </div>
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Auth;
--- clients/src/components/Navbar/NavBar.jsx
+++ clients/src/components/Navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import logo from "../Navbar/logo.ico";
@@ -10,20 +10,22 @@ import { IoIosNotifications } from "react-icons/io";
 import { gapi } from "gapi-script";
 import { IoApps } from "react-icons/io5";
 import { BiUserCircle } from "react-icons/bi";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { login } from "../../Actions/auth";
+import Auth from "../../Pages/Auth/Auth";
 
 function NavBar({ toggleDrawer }) {
+  const [AuthBtn, setAuthBtn] = useState(false)
   // const CurrentUser = null;
-  // const CurrentUser = {
-  //   result: {
-  //     name:"Durgesh Tiwari",
-  //     email: "dtiwari714@rku.ac.in",
-  //     joinedOn: "2222-07-15T09:57:23.489Z",
-  //   },
-  // };
-  const CurrentUser=useSelector(state=>state.currentUserReducer)
-  console.log(CurrentUser)
+  const CurrentUser = {
+    result: {
+      name:"Durgesh Tiwari",
+      email: "dtiwari714@rku.ac.in",
+      joinedOn: "2222-07-15T09:57:23.489Z",
+    },
+  };
+  // const CurrentUser = useSelector((state) => state.currentUserReducer);
+  console.log(CurrentUser);
   useEffect(() => {
     function start() {
       gapi.client
@@ -47,68 +49,77 @@ function NavBar({ toggleDrawer }) {
   const onSuccess = (response) => {
     const Email = response?.profileObj.email;
     console.log(Email);
-    dispatch(login({email:Email}))
+    dispatch(login({ email: Email }));
   };
 
   const onFailure = (response) => {
     console.log("Failed", response);
   };
   return (
-    <div className="Container_Navbar">
-      <div className="Burger_Logo_Navbar">
-        <div className="burger" onClick={() => toggleDrawer()}>
-          <p></p>
-          <p></p>
-          <p></p>
+    <>
+      <div className="Container_Navbar">
+        <div className="Burger_Logo_Navbar">
+          <div className="burger" onClick={() => toggleDrawer()}>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+
+          <Link to={"/"} className="logo_div_Navbar">
+            <img src={logo} alt="" />
+            <p className="logo_title_navbar">YouTube</p>
+          </Link>
+        </div>
+        {/* <div className="Header_Search"> */}
+        <SearchBar />
+        {/* </div> */}
+        <div className="Header_icons">
+          <RiVideoAddLine size={22} className={"vid_bell_Navbar"} />
+          <IoApps size={22} className={"app_navbar"} />
+          <IoIosNotifications size={22} className={"notification_Navbar"} />
+          {/* <FaUserCircle size={22} className={"user_Navbar"} /> */}
         </div>
 
-        <Link to={"/"} className="logo_div_Navbar">
-          <img src={logo} alt="" />
-          <p className="logo_title_navbar">YouTube</p>
-        </Link>
-      </div>
-      {/* <div className="Header_Search"> */}
-      <SearchBar />
-      {/* </div> */}
-      <div className="Header_icons">
-        <RiVideoAddLine size={22} className={"vid_bell_Navbar"} />
-        <IoApps size={22} className={"app_navbar"} />
-        <IoIosNotifications size={22} className={"notification_Navbar"} />
-        {/* <FaUserCircle size={22} className={"user_Navbar"} /> */}
-      </div>
-
-      <div className="Auth_cont_Navbar">
-        {CurrentUser ? (
-          <>
-            <div className="Chanel_logo_App">
-              <p className="fstChar_logo_App">
-                {CurrentUser?.result.name ? (
-                  <>{CurrentUser?.result.name.charAt(0).toUpperCase()}</>
-                ) : (
-                  <>{CurrentUser?.result.email.charAt(0).toUpperCase()}</>
+        <div className="Auth_cont_Navbar">
+          {CurrentUser ? (
+            <>
+              <div className="Chanel_logo_App"  onClick={()=>setAuthBtn(true)}>
+                <p className="fstChar_logo_App">
+                  {CurrentUser?.result.name ? (
+                    <>{CurrentUser?.result.name.charAt(0).toUpperCase()}</>
+                  ) : (
+                    <>{CurrentUser?.result.email.charAt(0).toUpperCase()}</>
+                  )}
+                </p>
+              </div>
+            </>
+          ) : (
+            <>
+              <GoogleLogin
+                clientId={
+                  "350764630254-24a99f0k7r7loe703360gk8j0hk7ivhc.apps.googleusercontent.com"
+                }
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                render={(renderProps) => (
+                  <p onClick={renderProps.onClick} className="Auth_Btn">
+                    <BiUserCircle size={22} className={"user_navbar"} />
+                    <b className="button_signIn">Sign In</b>
+                  </p>
                 )}
-              </p>
-            </div>
-          </>
-        ) : (
-          <>
-            <GoogleLogin
-              clientId={
-                "350764630254-24a99f0k7r7loe703360gk8j0hk7ivhc.apps.googleusercontent.com"
-              }
-              onSuccess={onSuccess}
-              onFailure={onFailure}
-              render={(renderProps)=>(
-                <p onClick={renderProps.onClick} className="Auth_Btn">
-                <BiUserCircle size={22} className={"user_navbar"} />
-                <b className="button_signIn">Sign In</b>
-              </p>
-              )}
-            />
-          </>
-        )}
+              />
+            </>
+          )}
+        </div>
       </div>
-    </div>
+      {
+         AuthBtn &&
+        <Auth
+        setAuthBtn={setAuthBtn}
+        User={CurrentUser}
+        />
+      }
+    </>
   );
 }
 

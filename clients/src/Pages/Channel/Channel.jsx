f09fb736b8bf8fd2d73f46a5e24bb4c8c91eaf67
Durgesh Tiwari <dtiwari714@rku.ac.in>
2024-02-29 22:21:29 +0530

    Show video on your video and chanel page YouTube Clone

--- clients/src/Pages/Channel/Channel.jsx
+++ clients/src/Pages/Channel/Channel.jsx
@@ -1,46 +1,48 @@
 import React from "react";
 import LeftSidebar from "../../components/LeftSideBar/LeftSideBar";
-import vid from "../../components/Video/video.mp4";
+// import vid from "../../components/Video/video.mp4";
+import { useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
 import ShowVideoGrid from "../../components/ShowVideoGrid/ShowVideoGrid";
 import DescribeChannel from "./DescribeChannel";
 
 function Channel({ setEditCreateChanelBtn, setVidUploadPage }) {
   const { Cid } = useParams();
-  const vids = [
-    {
-      _id: 1,
-      video_src: vid,
-      Chanel: "62bafe6752cea35a6c30685f",
-      title: "video 1",
-      Uploder: "abc",
-      description: "description of  video 1",
-    },
-    {
-      _id: 2,
-      video_src: vid,
-      Chanel: "cdd",
-      title: "video 2",
-      Uploder: "abc",
-      description: "description of  video 2",
-    },
-    {
-      _id: 3,
-      video_src: vid,
-      Chanel: "add",
-      title: "video 3",
-      Uploder: "abc",
-      description: "description of  video 3",
-    },
-    {
-      _id: 4,
-      video_src: vid,
-      Chanel: "add",
-      title: "video 3",
-      Uploder: "abc",
-      description: "description of  video 3",
-    },
-  ];
+  const vids=useSelector(state=>state.videoReducer)?.data?.filter(q=>q?.videoChanel === Cid).reverse();
+  // const vids = [
+  //   {
+  //     _id: 1,
+  //     video_src: vid,
+  //     Chanel: "62bafe6752cea35a6c30685f",
+  //     title: "video 1",
+  //     Uploder: "abc",
+  //     description: "description of  video 1",
+  //   },
+  //   {
+  //     _id: 2,
+  //     video_src: vid,
+  //     Chanel: "cdd",
+  //     title: "video 2",
+  //     Uploder: "abc",
+  //     description: "description of  video 2",
+  //   },
+  //   {
+  //     _id: 3,
+  //     video_src: vid,
+  //     Chanel: "add",
+  //     title: "video 3",
+  //     Uploder: "abc",
+  //     description: "description of  video 3",
+  //   },
+  //   {
+  //     _id: 4,
+  //     video_src: vid,
+  //     Chanel: "add",
+  //     title: "video 3",
+  //     Uploder: "abc",
+  //     description: "description of  video 3",
+  //   },
+  // ];
   return (
     <div className="container_Pages_App">
       <LeftSidebar />
--- clients/src/Pages/YourVideo/YourVideo.jsx
+++ clients/src/Pages/YourVideo/YourVideo.jsx
@@ -1,51 +1,59 @@
 import React from "react";
-import vid from "../../components/Video/video.mp4";
+// import vid from "../../components/Video/video.mp4";
 import "./YourVideo.css";
 import LeftSideBar from "../../components/LeftSideBar/LeftSideBar";
 import ShowVideoGrid from "../../components/ShowVideoGrid/ShowVideoGrid";
+import { useSelector } from "react-redux";
 
 function YourVideo() {
-  const vids = [
-    {
-      _id: 1,
-      video_src: vid,
-      Chanel: "62bafe6752cea35a6c30685f",
-      title: "video 1",
-      Uploder: "abc",
-      description: "description of  video 1",
-    },
-    {
-      _id: 2,
-      video_src: vid,
-      Chanel: "cdd",
-      title: "video 2",
-      Uploder: "abc",
-      description: "description of  video 2",
-    },
-    {
-      _id: 3,
-      video_src: vid,
-      Chanel: "add",
-      title: "video 3",
-      Uploder: "abc",
-      description: "description of  video 3",
-    },
-    {
-      _id: 4,
-      video_src: vid,
-      Chanel: "add",
-      title: "video 3",
-      Uploder: "abc",
-      description: "description of  video 3",
-    },
-  ];
+  const CurrentUser = useSelector((state) => state?.currentUserReducer);
+  const vids=useSelector(state=>state.videoReducer)?.data?.filter(q=>q?.videoChanel === CurrentUser?.result?._id).reverse();
+  // const vids = [
+  //   {
+  //     _id: 1,
+  //     video_src: vid,
+  //     Chanel: "62bafe6752cea35a6c30685f",
+  //     title: "video 1",
+  //     Uploder: "abc",
+  //     description: "description of  video 1",
+  //   },
+  //   {
+  //     _id: 2,
+  //     video_src: vid,
+  //     Chanel: "cdd",
+  //     title: "video 2",
+  //     Uploder: "abc",
+  //     description: "description of  video 2",
+  //   },
+  //   {
+  //     _id: 3,
+  //     video_src: vid,
+  //     Chanel: "add",
+  //     title: "video 3",
+  //     Uploder: "abc",
+  //     description: "description of  video 3",
+  //   },
+  //   {
+  //     _id: 4,
+  //     video_src: vid,
+  //     Chanel: "add",
+  //     title: "video 3",
+  //     Uploder: "abc",
+  //     description: "description of  video 3",
+  //   },
+  // ];
   return (
     <div className="container_Pages_App">
       <LeftSideBar />
       <div className="container2_Pages_App">
         <div className="container_yourvideo">
           <h1>Your Video</h1>
-          {<ShowVideoGrid vids={vids} />}
+          {
+            CurrentUser ?(<>
+          <ShowVideoGrid vids={vids} />
+            </>):<>
+            <h3>Plz Login to see Your uploded video list</h3></>
+          }
         </div>
       </div>
     </div>
